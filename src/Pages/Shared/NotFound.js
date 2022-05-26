d100fce60bbf2601a1a5be2043f34cf7fcea3358
Naimur Rahib <61423411+7rahib@users.noreply.github.com>
2022-05-26 23:25:47 +0600

    404 image addede

--- src/Pages/Shared/NotFound.js
+++ src/Pages/Shared/NotFound.js
@@ -3,10 +3,10 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div className='min-h-screen flex justify-center items-center'>
-            <div>
-                <h1 className='text-center bg-base-300 text-error text-7xl p-5 mask mask-circle'>!</h1>
+            <div className=''>
+                <img src="https://i.ibb.co/nrvR1sd/404-pages.jpg" alt="" />
                 <h1 className='text-error text-4xl font-semibold text-center my-3'>Error</h1>
-                <p className='text-warning text-2xl'>The Page you are looking is not found</p>
+                <p className='text-warning text-2xl text-center'>The Page you are looking is not found</p>
             </div>
         </div>
     );
